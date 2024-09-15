db0bdfa4a6acf44e1816f3e91cce218b6e689a57
phliem <philipp.liehm@gmail.com>
2024-09-15 09:12:16 +0200

    Player is updated, draw, can be controlled with the keys

--- main.py
+++ main.py
@@ -3,11 +3,23 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
+
 
 def main():
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable,drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         # end game
@@ -17,8 +29,14 @@ def main():
         
         # draw black    
         screen.fill((0,0,0))
-        # refresh
+        for element in updatable:
+            element.update(dt)
+        for element in drawable:
+            element.draw(screen)
+        
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
